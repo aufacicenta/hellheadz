ab4e08b9fa7e7ce77ab57ab97db4e97ec41a897e
netpoe <gus@aufacicenta.com>
2024-05-10 10:47:48 -0600

    refactor: removes purchasing tests

--- hardhat/contracts/LarsKristoHellheadz.sol
+++ hardhat/contracts/LarsKristoHellheadz.sol
@@ -3,39 +3,14 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Royalty.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract LarsKristoHellheadz is ERC721Enumerable, ERC721Royalty, ReentrancyGuard {
-  error ERC721InvalidPrice(uint256 tokenId, uint256 price);
-  error ERC721InvalidPurchaseAmount(uint256 tokenId, uint256 price, uint256 balance);
+contract LarsKristoHellheadz is ERC721Enumerable, ERC721Royalty, Ownable {
   error ERC721ForbiddenMint(uint256 tokenId);
 
-  event Purchase(
-    address indexed from,
-    address indexed to,
-    uint256 indexed tokenId,
-    uint256 price,
-    uint256 royaltyAmount,
-    uint256 transactionFee,
-    uint256 amount
-  );
-
-  event SetTokenForSale(address indexed owner, uint256 indexed tokenId, uint256 price);
-
-  modifier onlyAuthor() {
-    if (_msgSender() != _author) {
-      revert ERC721InvalidOwner(_msgSender());
-    }
-    _;
-  }
-
   address private _author; // larskristo.eth
-  address private _operator; // svpervnder.eth
 
   uint256 private _tokenLimit; // set to 666 at build time
-
-  mapping(uint256 tokenId => uint256) private _tokenPrices;
-  uint256 private _transactionFraction = 300; // 3%
 
   string[] private _tokenURIs;
 
@@ -43,17 +18,15 @@ contract LarsKristoHellheadz is ERC721Enumerable, ERC721Royalty, ReentrancyGuard
     string memory name_,
     string memory symbol_,
     address author,
-    address operator,
     uint256 tokenLimit
-  ) ERC721(name_, symbol_) {
+  ) ERC721(name_, symbol_) Ownable(author) {
     _author = author;
-    _operator = operator;
     _tokenLimit = tokenLimit;
 
     _setDefaultRoyalty(_author, 1000); // 10% royalty
   }
 
-  function mintUntilDoomsday(string[] memory tokenURIs_) public onlyAuthor {
+  function batchMint(string[] memory tokenURIs_) public onlyOwner {
     for (uint i = 0; i < tokenURIs_.length; i++) {
       uint256 tokenId = totalSupply();
       uint256 nextTokenId = tokenId + 1;
@@ -66,87 +39,7 @@ contract LarsKristoHellheadz is ERC721Enumerable, ERC721Royalty, ReentrancyGuard
 
       string memory tokenURI_ = tokenURIs_[i];
       _tokenURIs.push(tokenURI_);
-
-      _tokenPrices[tokenId] = 0.5 ether; // initial token price
     }
-  }
-
-  function buyToken(uint256 tokenId) public payable nonReentrant returns (uint256, uint256, uint256) {
-    uint256 price = _requireTokenPriceSet(tokenId);
-    uint256 balance = msg.value;
-
-    if (balance != price) {
-      revert ERC721InvalidPurchaseAmount(tokenId, price, balance);
-    }
-
-    address previousOwner = ownerOf(tokenId);
-    if (previousOwner == address(0)) {
-      revert ERC721InvalidOwner(previousOwner);
-    }
-
-    (address royaltyReceiver, uint256 royaltyAmount) = royaltyInfo(tokenId, price);
-    uint256 transactionFee = getTransactionFee(tokenId);
-    uint256 priceMinusFees = price - royaltyAmount - transactionFee;
-
-    // pay royalties to author
-    payable(royaltyReceiver).transfer(royaltyAmount);
-    // charge the transaction fee
-    payable(_operator).transfer(transactionFee);
-    // transfer the payment to the previous owner
-    payable(previousOwner).transfer(priceMinusFees);
-
-    // transfer the token to the new owner
-    _safeTransfer(previousOwner, _msgSender(), tokenId);
-
-    // reset the token price
-    _tokenPrices[tokenId] = 0;
-
-    emit Purchase(previousOwner, _msgSender(), tokenId, price, royaltyAmount, transactionFee, priceMinusFees);
-
-    return (tokenId, price, balance);
-  }
-
-  /**
-   * @dev Retrieves the price of a token.
-   * @param tokenId The ID of the token.
-   * @return The price of the token.
-   */
-  function getTokenPrice(uint256 tokenId) public view returns (uint256) {
-    _requireTokenPriceSet(tokenId);
-
-    return _tokenPrices[tokenId];
-  }
-
-  /**
-   * @dev Retrieves the transaction fee for a given token ID.
-   * @param tokenId The ID of the token.
-   * @return The transaction fee amount.
-   */
-  function getTransactionFee(uint256 tokenId) public view returns (uint256) {
-    uint256 _price = _requireTokenPriceSet(tokenId);
-
-    uint256 _transactionFee = (_price * _transactionFraction) / _feeDenominator();
-
-    return _transactionFee;
-  }
-
-  /**
-   * @dev Sets the token for sale with the specified price.
-   *
-   * Requirements:
-   * - The caller must be the owner of the token.
-   *
-   * @param tokenId The ID of the token to set for sale.
-   * @param price The price at which to sell the token.
-   */
-  function setTokenForSale(uint256 tokenId, uint256 price) public {
-    address owner = ownerOf(tokenId);
-
-    _checkAuthorized(owner, _msgSender(), tokenId);
-
-    _tokenPrices[tokenId] = price;
-
-    emit SetTokenForSale(owner, tokenId, price);
   }
 
   /**
@@ -189,21 +82,5 @@ contract LarsKristoHellheadz is ERC721Enumerable, ERC721Royalty, ReentrancyGuard
    */
   function _baseURI() internal view virtual override returns (string memory) {
     return "https://blockchainassetregistry.infura-ipfs.io/ipfs/";
-  }
-
-  /**
-   * @dev Checks if the token price is set for a given token ID.
-   * @param tokenId The ID of the token to check the price for.
-   * @return The price of the token.
-   * @dev Throws an error if the token price is not set.
-   */
-  function _requireTokenPriceSet(uint256 tokenId) internal view returns (uint256) {
-    uint256 _price = _tokenPrices[tokenId];
-
-    if (_price <= 0) {
-      revert ERC721InvalidPrice(tokenId, _price);
-    }
-
-    return _price;
   }
 }
--- hardhat/test/LarsKristoHellheadz.ts
+++ hardhat/test/LarsKristoHellheadz.ts
@@ -2,11 +2,7 @@
 /* eslint-disable node/no-unsupported-features/es-builtins */
 /* eslint-disable no-unused-expressions */
 import { expect } from "chai";
-import moment from "moment";
-import { BigNumber, BigNumberish } from "ethers";
-import { ethers, network } from "hardhat";
-import { DummyERC721, Lease as Lease721 } from "../typechain-types";
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { ethers } from "hardhat";
 
 const AUTHOR_ACCOUNT_ID = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
 const OPERATOR_ACCOUNT_ID = "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC";
@@ -38,13 +34,7 @@ async function createERC721Contract() {
 
   const ERC721 = await ethers.getContractFactory("LarsKristoHellheadz");
 
-  const contract = await ERC721.connect(signer).deploy(
-    TOKEN_NAME,
-    TOKEN_SYMBOL,
-    AUTHOR_ACCOUNT_ID,
-    OPERATOR_ACCOUNT_ID,
-    TOKEN_LIMIT,
-  );
+  const contract = await ERC721.connect(signer).deploy(TOKEN_NAME, TOKEN_SYMBOL, AUTHOR_ACCOUNT_ID, TOKEN_LIMIT);
 
   return contract;
 }
@@ -57,7 +47,7 @@ describe("LarskristoHellheadz", function () {
 
     const ERC721 = await createERC721Contract();
 
-    await ERC721.connect(author).mintUntilDoomsday(TOKEN_URIS);
+    await ERC721.connect(author).batchMint(TOKEN_URIS);
 
     const name = await ERC721.name();
     const symbol = await ERC721.symbol();
@@ -84,10 +74,6 @@ describe("LarskristoHellheadz", function () {
 
     // console.log({ token0, token1, token2, token3 });
 
-    // check for token price
-    const price = await ERC721.getTokenPrice(1);
-    expect(ethers.formatEther(price)).to.equal("0.5");
-
     // check approval for all
     await expect(ERC721.connect(author).setApprovalForAll(operator.address, true))
       .to.emit(ERC721, "ApprovalForAll")
@@ -105,156 +91,15 @@ describe("LarskristoHellheadz", function () {
     );
   });
 
-  it("getTokenPrice", async function () {
-    const [author] = await ethers.getSigners();
-
-    const ERC721 = await createERC721Contract();
-    await ERC721.connect(author).mintUntilDoomsday(TOKEN_URIS);
-
-    const price = await ERC721.getTokenPrice(1);
-
-    // console.log({ price: ethers.formatEther(price) });
-
-    expect(ethers.formatEther(price)).to.equal("0.5");
-  });
-
-  it("royaltyInfo", async function () {
-    const [author] = await ethers.getSigners();
-
-    const ERC721 = await createERC721Contract();
-    await ERC721.connect(author).mintUntilDoomsday(TOKEN_URIS);
-
-    const price = await ERC721.getTokenPrice(1);
-
-    const royaltyInfo = await ERC721.royaltyInfo(1, price);
-
-    // console.log({ price: ethers.formatEther(price), royaltyInfo });
-
-    const [receiver, royaltyAmount] = royaltyInfo;
-
-    expect(ethers.formatEther(royaltyAmount)).to.equal("0.05");
-    expect(receiver).to.equal(author.address);
-  });
-
-  it("setTokenForSale", async function () {
-    const [author, , operator, someoneElse] = await ethers.getSigners();
-
-    const ERC721 = await createERC721Contract();
-    await ERC721.connect(author).mintUntilDoomsday(TOKEN_URIS);
-
-    await ERC721.connect(author).setApprovalForAll(operator.address, true);
-
-    await ERC721.connect(operator).setTokenForSale(1, BigInt(ethers.parseEther("0.6")));
-
-    const price1 = await ERC721.getTokenPrice(1);
-
-    // console.log({ price1: ethers.formatEther(price1) });
-
-    expect(ethers.formatEther(price1)).to.equal("0.6");
-
-    await ERC721.connect(author).setTokenForSale(2, BigInt(ethers.parseEther("0.6")));
-
-    const price2 = await ERC721.getTokenPrice(2);
-
-    // console.log({ price2: ethers.formatEther(price2) });
-
-    expect(ethers.formatEther(price2)).to.equal("0.6");
-
-    await expect(ERC721.connect(someoneElse).setTokenForSale(2, BigInt(ethers.parseEther("0.6")))).to.be.reverted;
-  });
-
-  it("buyToken", async function () {
-    const [author, someoneElse, operator, buyer] = await ethers.getSigners();
-
-    // let authorBalance = await ethers.provider.getBalance(author.address);
-    // let operatorBalance = await ethers.provider.getBalance(operator.address);
-    // let buyerBalance = await ethers.provider.getBalance(buyer.address);
-
-    // console.log({
-    //   authorBalance: ethers.formatEther(authorBalance),
-    //   operatorBalance: ethers.formatEther(operatorBalance),
-    //   buyerBalance: ethers.formatEther(buyerBalance),
-    // });
-
-    const ERC721 = await createERC721Contract();
-    await ERC721.connect(author).mintUntilDoomsday(TOKEN_URIS);
-
-    await ERC721.connect(author).setApprovalForAll(operator.address, true);
-
-    const price = ethers.parseEther("1");
-
-    await ERC721.connect(operator).setTokenForSale(1, price);
-
-    // authorBalance = await ethers.provider.getBalance(author.address);
-    // operatorBalance = await ethers.provider.getBalance(operator.address);
-    // buyerBalance = await ethers.provider.getBalance(buyer.address);
-
-    // console.log({
-    //   authorBalance: ethers.formatEther(authorBalance),
-    //   operatorBalance: ethers.formatEther(operatorBalance),
-    //   buyerBalance: ethers.formatEther(buyerBalance),
-    // });
-
-    ERC721.on("Transfer", (from, to, tokenId) => {
-      // console.log({ from, to, tokenId });
-      expect(from).to.equal(author.address);
-      expect(to).to.equal(buyer.address);
-      expect(tokenId).to.equal(1);
-    });
-
-    const authorFee = ethers.parseEther("0.1");
-    const operatorFee = ethers.parseEther("0.03");
-    const sellerProfit = ethers.parseEther("0.87");
-
-    await expect(ERC721.connect(buyer).buyToken(1, { value: price }))
-      .to.emit(ERC721, "Purchase")
-      .withArgs(author.address, buyer.address, 1, price, authorFee, operatorFee, sellerProfit);
-
-    // console.log({ authorFee, price, percentage: (authorFee * 100n) / price });
-    expect((authorFee * 100n) / price).to.equal(10n);
-    expect((operatorFee * 100n) / price).to.equal(3n);
-    expect(price - authorFee - operatorFee).to.equal(sellerProfit);
-
-    const ownerOf0 = await ERC721.ownerOf(1);
-
-    // console.log({ ownerOf0 });
-
-    expect(ownerOf0).to.equal(buyer.address);
-
-    // authorBalance = await ethers.provider.getBalance(author.address);
-    // operatorBalance = await ethers.provider.getBalance(operator.address);
-    // buyerBalance = await ethers.provider.getBalance(buyer.address);
-
-    // console.log({
-    //   authorBalance: ethers.formatEther(authorBalance),
-    //   operatorBalance: ethers.formatEther(operatorBalance),
-    //   buyerBalance: ethers.formatEther(buyerBalance),
-    // });
-
-    // only the buyer should set prices now
-    await expect(ERC721.connect(author).setTokenForSale(1, BigInt(ethers.parseEther("2")))).to.be.reverted;
-    await expect(ERC721.connect(operator).setTokenForSale(1, BigInt(ethers.parseEther("2")))).to.be.reverted;
-
-    await expect(ERC721.connect(buyer).setTokenForSale(1, BigInt(ethers.parseEther("2"))))
-      .to.emit(ERC721, "SetTokenForSale")
-      .withArgs(buyer.address, 1, BigInt(ethers.parseEther("2")));
-
-    const newPrice = await ERC721.getTokenPrice(1);
-    expect(ethers.formatEther(newPrice)).to.equal("2.0");
-
-    // too poor to buy
-    await expect(ERC721.connect(someoneElse).buyToken(1, { value: BigInt(ethers.parseEther("1")) })).to.be.reverted;
-  });
-
-  it("mintUntilDoomsday", async function () {
+  it("batchMint", async function () {
     const [author, , , someoneElse] = await ethers.getSigners();
 
     const ERC721 = await createERC721Contract();
-    await ERC721.connect(author).mintUntilDoomsday(TOKEN_URIS);
+    await ERC721.connect(author).batchMint(TOKEN_URIS);
 
     let newTokenURI = "new-token-uri";
 
-    await ERC721.connect(author).mintUntilDoomsday([newTokenURI]);
+    await ERC721.connect(author).batchMint([newTokenURI]);
 
     let tokenUri = await ERC721.tokenURI(5);
     expect(tokenUri).to.equal(`https://blockchainassetregistry.infura-ipfs.io/ipfs/${newTokenURI}`);
@@ -262,14 +107,14 @@ describe("LarskristoHellheadz", function () {
     let totalSupply = await ERC721.totalSupply();
     expect(totalSupply).to.equal(6);
 
-    await expect(ERC721.connect(someoneElse).mintUntilDoomsday([newTokenURI])).to.be.revertedWithCustomError(
+    await expect(ERC721.connect(someoneElse).batchMint([newTokenURI])).to.be.revertedWithCustomError(
       ERC721,
-      `ERC721InvalidOwner`,
+      `OwnableUnauthorizedAccount`,
     );
 
     newTokenURI = "new-token-uri-2";
 
-    await ERC721.connect(author).mintUntilDoomsday([newTokenURI]);
+    await ERC721.connect(author).batchMint([newTokenURI]);
 
     tokenUri = await ERC721.tokenURI(6);
     expect(tokenUri).to.equal(`https://blockchainassetregistry.infura-ipfs.io/ipfs/${newTokenURI}`);
@@ -277,7 +122,7 @@ describe("LarskristoHellheadz", function () {
     totalSupply = await ERC721.totalSupply();
     expect(totalSupply).to.equal(7);
 
-    await expect(ERC721.connect(author).mintUntilDoomsday([newTokenURI])).to.be.revertedWithCustomError(
+    await expect(ERC721.connect(author).batchMint([newTokenURI])).to.be.revertedWithCustomError(
       ERC721,
       `ERC721ForbiddenMint`,
     );
