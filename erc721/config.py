3d29006ab2a7a47ea5946e533373af7f1b09847b
netpoe <gus@aufacicenta.com>
2024-07-23 12:05:04 -0600

    feat: batch 22 to 44

--- erc721/config.py
+++ erc721/config.py
@@ -13,22 +13,23 @@ open_ai_client = OpenAI(
 
 claude_anthropic_client = anthropic.Anthropic(api_key=os.getenv("ANTHROPICAI_API_KEY"))
 
-src_dir_path = "./files/hellheadz/20240523_0-21"
-thumbnails_dir_path = "./files/hellheadz/20240523_0-21/thumbnails"
-websize_dir_path = "./files/hellheadz/20240523_0-21/websize"
-watermarks_dir_path = "./files/hellheadz/20240523_0-21/watermarked"
-token_uris_dir_path = "./files/hellheadz/20240523_0-21"
+src_dir_path = "./files/hellheadz/20240723_22-44"
+originals_dir_path = f"{src_dir_path}/originals"
+thumbnails_dir_path = f"{src_dir_path}/thumbnails"
+websize_dir_path = f"{src_dir_path}/websize"
+watermarks_dir_path = f"{src_dir_path}/watermarked"
+token_uris_dir_path = f"{src_dir_path}"
 
 thumbnail_extension = "_thumbnail"
 
-metadata_file_path = "metadata/hellheadz/20240523_metadata-batch-0-21.json"
-metadata_ai_descriptions_file_name = "20240523_metadata-batch-0-21.json"
+metadata_file_path = "metadata/hellheadz/20240723_metadata-batch-22-44.json"
+metadata_ai_descriptions_file_name = "20240723_metadata-batch-22-44.json"
 
 watermark_text = "LarsKristo"
 author_text = "LarsKristo"
 generic_description_text = "Larskristo Hellheadz"
 
-token_uris_filename = f"{token_uris_dir_path}/20240523_token-uris-batch-0-21.json"
+token_uris_filename = f"{token_uris_dir_path}/20240723_token-uris-batch-22-44.json"
 
 ipfs_gateway_url = "https://blockchainassetregistry.infura-ipfs.io/ipfs/"
 
